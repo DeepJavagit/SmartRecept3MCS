a34f43984ee8342d3f138ddd4df7883df333ff72
pariharji <131267316+pariharji@users.noreply.github.com>
2025-04-05 22:22:47 +0530

    updating file

--- SmartReceptAPI/src/main/java/com/SmartReceptAPI/repo/CoustmerReceptsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.SmartReceptAPI.repo;
-
-import com.SmartReceptAPI.entity.CoustmerRecepts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoustmerReceptsRepository extends JpaRepository<CoustmerRecepts, Long> {
-}
--- SmartReceptAPI/src/main/java/com/SmartReceptAPI/repo/RecpExpensesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.SmartReceptAPI.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.SmartReceptAPI.entity.RecpExpenses;
-
-public interface RecpExpensesRepository extends JpaRepository<RecpExpenses, Long> {
-    RecpExpenses findByName(String name);
-}
